0ccae18cc938a8c91c9b7e905f9234e03ec3bfcf
Darius Rod <77187129+oocheen@users.noreply.github.com>
2022-01-13 19:37:07 +1100

    settings changesd for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 
+# from django.core.management.utils import get_random_secret_key
+# get_random_secret_key()
 
+# print(get_random_secret_key())
+
+# with these lines above you can generate a new secret key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,14 +28,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','mcvc1cs@fhf3shj&$1sp=l=nj^t2r1=1%55pe9x0eghfukm!9m')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','mcvc1cs@fhf3shj&$1sp=l=nj^t2r1=1%55pe9x0eghfukm!9m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) =="1" #1 equals True
 
 ALLOWED_HOSTS = []
 if not DEBUG:
-    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
+    ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
 
 # Application definition
 
@@ -88,6 +93,32 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+POSTGRES_DB = os.environ.get("POSTGRES_DB")# database name
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")#database user password
+POSTGRES_USER = os.environ.get("POSTGRES_USER")#database username
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")# database host
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")# database port 
+
+POSTGRES_READY = (
+    POSTGRES_DB is not None
+    and POSTGRES_PASSWORD is not None
+    and POSTGRES_USER is not None
+    and POSTGRES_HOST is not None
+    and POSTGRES_PORT is not None
+)
+
+if POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": POSTGRES_HOST,
+            "PORT": POSTGRES_PORT,
+        }
+    }
 
 
 # Password validation
